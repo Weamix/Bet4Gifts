c99acdf6935956bbd24ed9710b90df1bb1edbee5
Weamix <weamix16@gmail.com>
2018-04-18 13:35:14 +0200

    Update points.php

--- points.php
+++ points.php
@@ -1,25 +1,25 @@
 <?php
-function rang($pseudo)
+function point($pseudo, $pt)
 {
   include('/index.php');
 
   //on cherche si le pseudo existe
-  $sql = $bdd->prepare('SELECT pseudo FROM membre WHERE pseudo = ?');
+    $sql = $bdd->prepare('SELECT pseudo FROM membre WHERE pseudo = ?');
     $sql->execute(array($pseudo));   
     $req = $sql->fetch();
     if ($req['pseudo']) {
-        //on cherche le rang
-        $sql = $bdd->prepare('SELECT COUNT(*) + 1 AS rang FROM membre WHERE points > (SELECT points FROM membre WHERE pseudo = ?)');
-        $sql->execute(array($pseudo));   
-        $req = $sql->fetch();
-
-        $sql = $bdd->query('SELECT COUNT(*) AS nb_membre FROM membre');
-        $req_membre = $sql->fetch();
-
-        return array($req['rang'], $req_membre['nb_membre']);        
-    }
-    else {
-        return FALSE;
+    	//on selectionne le nombre de point
+    $sql = $bdd->prepare('SELECT points FROM membre WHERE pseudo = ?');
+    	$sql->execute(array($pseudo));   
+    	$req = $sql->fetch();
+    	//on met tout sous forme de nombre entier
+    	$point = (int) $req['points']; 
+    	$pt = (int) $pt;
+    	//on ajoute les points gagner
+    	$point += $pt;
+    	//on sauve le tout dans la bdd
+    	$sql = $bdd->prepare('UPDATE membre SET points = ? WHERE id = ?');
+        $sql->execute(array($point, $pseudo));    	
     }
 }
 ?>
