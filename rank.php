e0f7c0338f5307da18f38844c4076f23183c616d
Weamix <weamix16@gmail.com>
2018-04-18 13:53:37 +0200

    Update rank.php

--- rank.php
+++ rank.php
@@ -1,3 +1,14 @@
 <?php
-$reponse = $bdd->query('SELECT pseudo, points FROM membre ORDER BY points DESC LIMIT 100');
+mysql_connect("localhost", "root", "");
+mysql_select_db("personnages");
+$reponse = mysql_query('SELECT * FROM personnages ORDER BY experience');
+$rang = 0;
+ 
+while ($donnees = mysql_fetch_array($reponse))
+{
+$rang++;
+echo $rang;
+echo $donnees['pseudo'];
+echo $donnees['experience'];
+}
 ?>
