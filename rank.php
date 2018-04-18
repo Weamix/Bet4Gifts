cd2092f0065c45900f8a47bc82f9ef87803f24f4
Weamix <weamix16@gmail.com>
2018-04-18 13:57:56 +0200

    Update rank.php

--- rank.php
+++ rank.php
@@ -1,35 +1,23 @@
 <?php 
 
 // on se connecte à MySQL (à remplir) 
-$db = mysql_connect('localhost', 'login', 'password'); 
+$db = mysql_connect('localhost', 'isnprojet', 'O1cuz98@'); 
 // on sélectionne la base (à remplir) 
-mysql_select_db('nom_de_la_base',$db);  
+mysql_select_db('isnmpweb_espace_membre',$db);  
 
 
 // Tableau des meilleurs posteurs 
-$recup_posteurs =mysql_query( 'SELECT DISTINCT `username`, `visits`, `referals` FROM `tb_users` ORDER BY `visits` DESC LIMIT 0, 10 ');  
+$recup_posteurs =mysql_query( 'SELECT pseudo, points FROM membre ORDER BY points DESC LIMIT 100 ');  
 
 $compteur = 1; 
 while($best_posteurs = mysql_fetch_array($recup_posteurs)){ 
      // Mise en forme des données à revoir 
      echo $compteur."<br/>";      
-     echo $best_posteurs ["username"]."<br/>"; 
-     echo $best_posteurs ["visits"]."<br/>"; 
-     echo $best_posteurs ["referals"]."<br/>"; 
+     echo $best_posteurs ["pseudo"]."<br/>"; 
+     echo $best_posteurs ["points"]."<br/>"; 
+     
      $compteur++; 
 } 
 
-// Tableau des meilleurs parrains 
-$recup_parrains =mysql_query( 'SELECT DISTINCT `username`, `visits`, `referals` FROM `tb_users` ORDER BY `referals` DESC LIMIT 0, 10 ');  
-
-$compteur = 1; 
-while($best_parrains = mysql_fetch_array($recup_posteurs)){ 
-     // Mise en forme des données à revoir 
-     echo $compteur."<br/>";      
-     echo $best_parrains["username"]."<br/>"; 
-     echo $best_parrains["visits"]."<br/>"; 
-     echo $best_parrains["referals"]."<br/>"; 
-     $compteur++; 
-} 
 
 ?>
