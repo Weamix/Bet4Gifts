fd4d50b03fbb73a5fd330e5bca47c6bbbce181d1
Weamix <weamix16@gmail.com>
2018-04-18 13:29:00 +0200

    Update points.php

--- points.php
+++ points.php
@@ -1,3 +1,5 @@
+<?php
+  
 function point($pseudo, $pt)
 {
   //on cherche si le pseudo existe
@@ -5,17 +7,23 @@ function point($pseudo, $pt)
     $sql->execute(array($pseudo));   
     $req = $sql->fetch();
     if ($req['pseudo']) {
-    	//on selectionne le nombre de point
+    	//on selectionne le nombre de points
     $sql = $bdd->prepare('SELECT points FROM membre WHERE pseudo = ?');
     	$sql->execute(array($pseudo));   
     	$req = $sql->fetch();
     	//on met tout sous forme de nombre entier
     	$point = (int) $req['points']; 
     	$pt = (int) $pt;
-    	//on ajoute les points gagner
+    	//on ajoute les points gagnés
     	$point += $pt;
     	//on sauve le tout dans la bdd
     	$sql = $bdd->prepare('UPDATE membre SET points = ? WHERE id = ?');
         $sql->execute(array($point, $pseudo));    	
     }
 }
+
+
+<?php } ?>
+
+
+
