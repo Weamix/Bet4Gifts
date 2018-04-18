5f5b92f7c5c7e34e67aaecdd1504f2657b05a161
Weamix <weamix16@gmail.com>
2018-04-18 14:03:41 +0200

    Update rank.php

--- rank.php
+++ rank.php
@@ -5,7 +5,16 @@ $db = mysql_connect('localhost', 'isnprojet', 'O1cuz98@');
 // on sélectionne la base (à remplir) 
 mysql_select_db('isnmpweb_espace_membre',$db);  
 
-
 // Tableau des meilleurs 
-$recup_id =mysql_query( 'SELECT pseudo, points FROM membre ORDER BY points DESC LIMIT 100 ');  
+$recup_id =mysql_query( 'SELECT pseudo, points FROM membre ORDER BY points DESC LIMIT 100 '); 
+
+$compteur = 1; 
+while($best_parrains = mysql_fetch_array($recup_posteurs)){ 
+     // Mise en forme des données à revoir 
+     echo $compteur."<br/>";      
+     echo $best_membres["username"]."<br/>"; 
+     echo $best_membres["points"]."<br/>"; 
+        $compteur++; 
+} 
+
 ?>
