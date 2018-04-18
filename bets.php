c16c3e3a203ff6167fddfb971dbfbdb6257d8950
Elkios <pomier.mathys@outlook.fr>
2018-04-18 20:31:26 +0200

    added bets.php

---
+++ bets.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>Bets</title>
+  </head>
+  <body>
+
+      <form class="" action="" method="post">
+
+        <label for=""> Equipe 1
+          <input type="checkbox" name="1" value="">
+        </label>
+        <label for=""> Match Nul
+          <input type="checkbox" name="0" value="">
+        </label>
+        <label for=""> Equipe 2
+          <input type="checkbox" name="2" value="">
+        </label>
+        <label for=""> Montant :
+          <input type="number" name="montant" value=""> <input type="submit" name="" value="">
+        </label>
+
+      </form>
+
+  </body>
+</html>
