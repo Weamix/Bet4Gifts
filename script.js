39e78b9979c955f9fd85cd01f8ea1a0b03bd4c5c
Weamix <weamix16@gmail.com>
2018-04-19 14:22:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function resizeHeaderHeight(){
 
 	var random = Math.ceil(Math.random()*3);
 
-	document.getElementById("full-background").style.height =  window.innerHeight + "px";
+	document.getElementById("full-background").style.height =  window.innerHeight + "px"; // s'adapte à la taille de l'écran
 	document.getElementById("full-background").style.backgroundImage = "url(images/background" + random +".jpg)";
 
 
