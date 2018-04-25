441a4a42762d4a7e4a6e808aeb4adb1fae0601fc
Elkios <pomier.mathys@outlook.fr>
2018-04-25 18:55:48 +0200

    update test.php

--- test.php
+++ test.php
@@ -8,7 +8,7 @@
     $message='
     <html>
        <body style="margin: 0; padding: 0; background-color:#2c3e50;">
-          <div style="margin-top: 50px;" align="center"><img src="https://bet4gifts.web-edu.fr/images/favicon.png" alt="" width="200" height="200"></div>
+          <div style="margin: 50px 0 0 0;" align="center"><img src="https://bet4gifts.web-edu.fr/images/favicon.png" alt="" width="200" height="200"></div>
 
           <div align="center">
              <a href="https://bet4gifts.web-edu.fr/confirmation.php?pseudo='.urlencode($pseudo).'&key='.$key.'">Confirmez votre compte !</a>
