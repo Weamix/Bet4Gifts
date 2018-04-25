e2f625dc6475ea7b0305ff2c8fefc3d74e216bf5
Elkios <pomier.mathys@outlook.fr>
2018-04-25 19:06:45 +0200

    update test.php

--- test.php
+++ test.php
@@ -16,7 +16,7 @@
           <div align="center">
             <span style="display: block; color: #FFF; font-size: 20px;">Your account was successfully created!</span>
             <span style="display: block; color: #FFF; font-size: 20px;">You are just one step to confirm your mail !</span>
-            <a style="background-color: #27ae60; border-radius: 20px; color: #FFF; text-decoration: none; font-size: 15px;" href="https://bet4gifts.web-edu.fr/confirmation.php?pseudo='.urlencode($pseudo).'&key='.$key.'">Confirmez votre compte !</a>
+            <a style="background-color: #27ae60; border-radius: 20px; padding: 16px 18px; color: #FFF; text-decoration: none; font-size: 15px;" href="https://bet4gifts.web-edu.fr/confirmation.php?pseudo='.urlencode($pseudo).'&key='.$key.'">Confirmez votre compte !</a>
           </div>
        </body>
     </html>
