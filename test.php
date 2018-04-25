b654b5fbd11b3210e49de50cda2421d5426b8dfe
Elkios <pomier.mathys@outlook.fr>
2018-04-25 19:18:33 +0200

    update test.php

--- test.php
+++ test.php
@@ -23,6 +23,7 @@
           <div align="center">
             <a style="background-color: #27ae60; border-radius: 20px; padding: 16px 18px; color: #FFF; text-decoration: none; font-size: 15px;" href="https://bet4gifts.web-edu.fr/confirmation.php?pseudo='.urlencode($pseudo).'&key='.$key.'">Click me !</a>
           </div>
+          <br><br><br>
        </body>
     </html>
     ';
