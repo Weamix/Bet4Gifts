3057694fe5f91b8b464a4b7087d0ad4f092a3887
Elkios <pomier.mathys@outlook.fr>
2018-04-26 14:03:41 +0200

    update

--- connexion.php
+++ connexion.php
@@ -66,7 +66,6 @@
       <input type="submit" name="formconnexion" value="Sign up">
 
       <span>No account ? <a href="inscription.php">Sign up!</a></span>
-      <br>
       <span>Forgotten password ? <a href="forgottenpassword.php">Change it !</a> !</span>
 
     </form>
