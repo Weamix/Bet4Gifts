e46b23f1549e899f421ed7a50a715bc4fee526e8
Elkios <pomier.mathys@outlook.fr>
2018-05-06 15:13:52 +0200

    update

--- bets.php
+++ bets.php
@@ -23,7 +23,7 @@
   </head>
   <body>
 
-   <?php //if(isset($_SESSION['id']) AND $_GET['user'] == $_SESSION['id']) { ?>
+   <?php if(isset($_SESSION['id']) AND $_GET['user'] == $_SESSION['id']) { ?>
 
       <form class="" action="test.php" method="post">
 
@@ -44,7 +44,7 @@
 
       </form>
 
-    <?php//}?>
+    <?php}?>
 
   </body>
 </html>
