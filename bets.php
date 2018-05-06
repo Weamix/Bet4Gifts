5d5186b9a7c9990abb7f35335485d19d7ed78a27
Elkios <pomier.mathys@outlook.fr>
2018-05-06 20:09:15 +0200

    update

--- bets.php
+++ bets.php
@@ -4,22 +4,6 @@
 
   $bdd = new PDO('mysql:host=localhost;dbname=isnmpweb_espace_membre', 'isnprojet', 'O1cuz98@');
 
-  if (isset($_GET['id'], $_GET['tone'], $_GET['ttwo'], $_GET['user']) AND $_GET['id'] > 0 AND !empty($_GET['tone']) AND !empty($_GET['ttwo'])) {
-
-    $getuserid = intval($_GET['user']);
-    $requser = $bdd->prepare("SELECT * FROM membres WHERE id = ?");
-    $requser->execute(array($getuserid));
-    $userinfo = $requser->fetch();
-
-    echo $userinfo['id'];
-    echo $userinfo['speudo'];
-
-
-
-  }else {
-    header("Location: index.php");
-  }
-
 ?>
 
 <!DOCTYPE html>
@@ -31,26 +15,7 @@
   </head>
   <body>
 
-   <?php if(isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id']) {?>
 
-      <form class="" action="test.php" method="post">
-
-        <label for=""> Equipe 1
-          <input type="radio" name="betcheck">
-        </label>
-        <label for=""> Match Nul
-          <input type="radio" name="betcheck" value="equality">
-        </label>
-        <label for=""> Equipe 2
-          <input type="radio" name="betcheck">
-        </label>
-        <label for=""> Montant :
-          <input type="number" name="amount" value="1" min="1" <?php echo 'max="'.$userinfo['points'].'"' ?>> <input type="submit" name="formbets" value="Parier !">
-        </label>
-
-      </form>
-
-    <?php} else{ header("Location: index.php"); } ?>
 
   </body>
 </html>
