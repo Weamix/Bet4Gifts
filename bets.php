b03a0e2eb7ea050786a97ad3ebccda3033a4f9d1
Elkios <pomier.mathys@outlook.fr>
2018-05-06 20:31:24 +0200

    update

--- bets.php
+++ bets.php
@@ -4,6 +4,26 @@
 
   $bdd = new PDO('mysql:host=localhost;dbname=isnmpweb_espace_membre', 'isnprojet', 'O1cuz98@');
 
+  if (isset($_GET['user'], $_GET['tone']), $_GET['ttwo'], $_GET['id'] AND $_GET['id'] > 0 AND $_GET['user'] > 0) AND !empty($_GET['tone']) AND !empty($_GET['ttwo'])) {
+
+    $userid = intval($_GET['user']);
+    $requser = $bdd->prepare("SELECT * FROM membres WHERE id = ?");
+    $requser->execute(array($userid));
+    $userinfo = $requser->fetch();
+
+    $matchid = intval($_GET['id']);
+    $matchteamone = $_GET['tone'];
+    $matchteamtwo = $_GET['ttwo'];
+    $reqmatch = $bdd->prepare("SELECT * FROM matches WHERE id = ? AND team_one = ? AND team_two = ?");
+    $reqmatch->execute(array($matchid, $matchteamone, $matchteamtwo));
+    $matchinfo = $reqmatch->fetch();
+
+  }else {
+
+    header('Location: index.php');
+
+  }
+
 ?>
 
 <!DOCTYPE html>
