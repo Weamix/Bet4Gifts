61e5db29d5d2b34a1045a68b1ffe4f4a936b3a1d
Elkios <pomier.mathys@outlook.fr>
2018-05-07 18:30:51 +0200

    update

--- bets.php
+++ bets.php
@@ -21,7 +21,7 @@
 
     $reqalreadybet = $bdd->prepare("SELECT * FROM bets WHERE match_id = ? AND team_one = ? AND team_two = ?");
     $reqalreadybet->execute(array($matchinfo['id'], $matchinfo['team_one'], $matchinfo['team_two']));
-    $alreadybet = 1;
+    $reqalreadybet->rowCount();
 
     if ($alreadybet == 0) {
 
@@ -64,9 +64,9 @@
 
       }
 
-    }/*else {
-      header("Location : index.php");
-    }*/
+    }else {
+      header("Location: index.php");
+    }
 
 
 ?>
