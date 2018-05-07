bd488926be2e14c3525990fa7c9e7240c378d6d0
Elkios <pomier.mathys@outlook.fr>
2018-05-07 15:55:53 +0200

    update

--- bets.php
+++ bets.php
@@ -5,7 +5,7 @@
 
   $bdd = new PDO('mysql:host=localhost;dbname=isnmpweb_espace_membre', 'isnprojet', 'O1cuz98@');
 
-  if (isset($_GET['id']) AND $_GET['id'] > 0) {
+  if (isset($_GET['user'], $_GET['tone'], $_GET['ttwo'], $_GET['id']) AND $_GET['user'] > 0 AND $_GET['id'] > 0 AND !empty($_GET['tone']) AND !empty($_GET['ttwo'])) {
 
     $getid = intval($_GET['id']);
     $requser = $bdd->prepare("SELECT * FROM membres WHERE id = ?");
@@ -26,51 +26,7 @@
 
     <?php if (isset($_SESSION['id']) AND $userinfo['id'] == $_SESSION['id']) { ?>
 
-      <h2>Your profile <?php echo $userinfo['pseudo']; ?></h2>
-      <br>
-      <span>Username : <?php echo $userinfo['pseudo']; ?></span>
-      <br>
-      <span>Email : <?php echo $userinfo['email']; ?></span>
-      <br>
-        <span>Points : <?php echo $userinfo['points']; ?></span>
-      <br>
-      <?php echo '<a href="bets.php?id='.$_GET['id'].'">Bet</a>'; ?>
-      <br>
-      <a href="#">Edit profil</a>
-      <br>
-      <a href="deconnexion.php">Sign out</a>
-
-      <div class="bet">
-
-        <h2>Matches avaible</h2>
-
-        <?php
-
-            $reqmatchavaible = $bdd->prepare("SELECT * FROM matches WHERE categories = ? AND match_start > CURDATE()");
-            $reqmatchavaible->execute(array("football"));
-
-            while ($matchavaible = $reqmatchavaible->fetch()) {
-
-              $date = $matchavaible['match_start'];
-              $match_start = date('d-m-Y H:i', strtotime($date));
-
-            ?>
-              <div class="bet_container">
-                <span><?php echo $matchavaible['team_one']; ?> VS <?php echo $matchavaible['team_two']; ?></span>
-                <br>
-                <br>
-                <span>Categorie : <?php echo $matchavaible['categories']; ?></span>
-                <span>Date: <?php echo $match_start; ?></span>
-                <br>
-                <a href="<?php echo 'bets.php?user='.$userinfo['id'].'&tone='.$matchavaible['team_one'].'&ttwo='.$matchavaible['team_two'].'&id='.$matchavaible['id']; ?>">Pariez !</a>
-              </div>
-        <?php } ?>
-
-
-      </div>
-
-
-
+        <p>TEST</p>
 
     <?php } ?>
 
