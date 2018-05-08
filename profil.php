732c96b3ca5a02ba76a5dc50bba4148f908602be
Elkios <pomier.mathys@outlook.fr>
2018-05-08 13:54:33 +0200

    update

--- profil.php
+++ profil.php
@@ -76,7 +76,7 @@
 
         <?php
 
-          $reqmatchbetincoming = $bdd->prepare('SELECT * FROM bets WHERE author_id = ? AND match_start < CURDATE()');
+          $reqmatchbetincoming = $bdd->prepare('SELECT * FROM bets WHERE author_id = ? AND match_start > CURDATE()');
           $reqmatchbetincoming->execute(array($userinfo['id']));
 
           while ($matchbetincoming = $reqmatchbetincoming->fetch()) {
