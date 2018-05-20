0a3dc4798e9f99b8a774f03278a6b104dd52b222
Elkios <pomier.mathys@outlook.fr>
2018-05-20 12:42:01 +0200

    deconnexion.php

--- deconnexion.php
+++ deconnexion.php
@@ -1,8 +1,8 @@
 <?php
 
-  session_start();
-  $_SESSION = array();
-  session_destroy();
-  header("Location: connexion.php");
+  session_start(); //permet d'utiliser les variables de SESSION
+  $_SESSION = array(); //On défini toutes les variables de session de l'utilisateur sur NULL
+  session_destroy(); //On détruit la SESSION
+  header("Location: connexion.php"); //On redirige l'utilisateur vers la page de connexion
 
  ?>
