df570f252bdb4308dccb1a9e4d30a57010dc2fb2
Weamix <weamix16@gmail.com>
2018-05-21 16:17:29 +0200

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,4 +1,5 @@
--<?php 
+<?php
+ini_set("display_errors",1);
 try {
     $db = new PDO('mysql:host=localhost;dbname=isnmpweb_espace_membre', 'isnprojet', 'O1cuz98@');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -7,22 +8,15 @@ try {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
 }
-$sql = $conn->query("SELECT nom_produit, prix FROM maboutique");
-$data = $sql->fetch();
-- 
--$array = array("cle_steam_aleatoire" => $sql['nom_produit'], "prix" => $sql['prix']);
--echo '<table>
--   <caption>La Boutique</caption>
--   <tr>
--       <th>Nom du produit</th>
--       <th>prix</th>
--       <th>action</th>
--   </tr>';
--foreach($array as $item){
--    echo '
--   <tr>
--       <td>'.$array['cle_steam_aleatoire'].'</td>
--       <td>'.$array['prix'].'points</td>
--       <td><a href="#">Acheter</a></td>
--   </tr><br />
--</table>';}?>
+$sql = $db->query("SELECT nom_produit, prix FROM maboutique");
+
+echo '<table border="1">';
+while($data = $sql->fetch())
+{
+   $nom_produit = $data['nom_produit'];
+
+echo "<tr><td>$nom_produit</td><td></td></tr>";
+
+}
+echo '</table>';
+?>
