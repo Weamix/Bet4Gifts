a0933be1e7aeb05efe1544f79a4ba7d9861b188d
Weamix <weamix16@gmail.com>
2018-05-21 13:17:30 +0200

    Create boutique.php

---
+++ boutique.php
@@ -0,0 +1,18 @@
+<?php $sql = mysql_query("SELECT nom_produit, prix FROM maboutique");
+ 
+$array = array("Nom_prod" => $sql['nom_produit'], "prix" => $sql['prix']);
+echo '<table>
+   <caption>La Boutique</caption>
+   <tr>
+       <th>Nom du produit</th>
+       <th>prix</th>
+       <th>action</th>
+   </tr>';
+foreach($array as $item){
+    echo '
+   <tr>
+       <td>'.$array['Nom_prod'].'</td>
+       <td>'.$array['prix'].'€</td>
+       <td><a href="#">Acheter</a></td>
+   </tr><br />
+</table>';}?>
