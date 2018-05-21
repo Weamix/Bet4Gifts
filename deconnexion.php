80273c6de95c0833f6960c08505feb856d032445
Weamix <weamix16@gmail.com>
2018-05-21 21:48:00 +0200

    Update deconnexion.php
    COMMENTED

--- deconnexion.php
+++ deconnexion.php
@@ -1,7 +1,7 @@
 <?php
 
   session_start(); //permet d'utiliser les variables de SESSION
-  $_SESSION = array(); //On défini toutes les variables de session de l'utilisateur sur NULL
+  $_SESSION = array(); //On définit toutes les variables de session de l'utilisateur sur NULL
   session_destroy(); //On détruit la SESSION
   header("Location: connexion.php"); //On redirige l'utilisateur vers la page de connexion
 
